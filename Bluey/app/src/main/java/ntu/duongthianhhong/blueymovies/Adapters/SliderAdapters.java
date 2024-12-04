1abd86feba0827b22bf5af425f57300d21cce00b
Anh Hong <samh5813p@gmail.com>
2024-12-04 09:28:23 +0700

    Giao diện slider

--- Bluey/app/src/main/java/ntu/duongthianhhong/blueymovies/Adapters/SliderAdapters.java
+++ Bluey/app/src/main/java/ntu/duongthianhhong/blueymovies/Adapters/SliderAdapters.java
@@ -2,10 +2,18 @@ package ntu.duongthianhhong.blueymovies.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 
 import java.util.List;
 
@@ -27,17 +35,44 @@ public class SliderAdapters extends RecyclerView.Adapter<SliderAdapters.SliderVi
     public SliderAdapters.SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         context=parent.getContext();
         return new SliderViewHolder(LayoutInflater.from(parent.getContext()).inflate(
-                R.layout
+                R.layout.slider_item_container,parent,false
         ));
     }
 
     @Override
     public void onBindViewHolder(@NonNull SliderAdapters.SliderViewHolder holder, int position) {
-
+        holder.setImage(sliderItems.get(position));
+        if(position==sliderItems.size()-2){
+            viewPager2.post(runnable);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return sliderItems.size();
     }
+
+    public class SliderViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageSlide);
+        }
+        void setImage(SliderItems sliderItems){
+            RequestOptions requestOptions= new RequestOptions();
+            requestOptions=requestOptions.transforms(new CenterCrop(), new RoundedCorners(60));
+
+            Glide.with(context)
+                    .load(sliderItems.getImage())
+                    .apply(requestOptions)
+                    .into(imageView);
+        }
+    }
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
 }
--- Bluey/app/src/main/java/ntu/duongthianhhong/blueymovies/Domains/SliderItems.java
+++ Bluey/app/src/main/java/ntu/duongthianhhong/blueymovies/Domains/SliderItems.java
@@ -3,6 +3,10 @@ package ntu.duongthianhhong.blueymovies.Domains;
 public class SliderItems {
     private int image;
 
+    public SliderItems(int image) {
+        this.image = image;
+    }
+
     public int getImage() {
         return image;
     }
