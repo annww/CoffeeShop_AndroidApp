a5d4b461bf7e31000db573699ef1e3fed8eb1127
Anh Hong <samh5813p@gmail.com>
2025-01-12 11:32:42 +0700

    update fragment

--- Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Activities/MainActivity.java
+++ Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Activities/MainActivity.java
@@ -1,26 +1,57 @@
 package ntu.duongthianhhong.blueymovies_64130758.Activities;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import ntu.duongthianhhong.blueymovies_64130758.R;
+import ntu.duongthianhhong.blueymovies_64130758.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new ExplorerFragment());
+        binding.bottomNavigationMenu.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.favorites) {
+                replaceFragment(new FavoriteFragment());
+            }
+            if(item.getItemId() == R.id.explorer) {
+                replaceFragment(new ExplorerFragment());
+            }
+            if(item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            }
+            if(item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
         });
+
+        // giúp layout của cửa sổ vượt ra ngoài giới hạn thông thường của màn hình.
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
