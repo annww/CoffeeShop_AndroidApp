b5e33a9eb0fff0c6af66021a0029dd2892bfce82
Anh Hong <samh5813p@gmail.com>
2025-01-12 11:58:27 +0700

    update giao diện explorer

--- Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Activities/ExplorerFragment.java
+++ Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Activities/ExplorerFragment.java
@@ -1,66 +1,217 @@
 package ntu.duongthianhhong.blueymovies_64130758.Activities;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
+import ntu.duongthianhhong.blueymovies_64130758.Domains.Film;
 import ntu.duongthianhhong.blueymovies_64130758.R;
+import ntu.duongthianhhong.blueymovies_64130758.databinding.FragmentExplorerBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ExplorerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ExplorerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ExplorerFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ExplorerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ExplorerFragment newInstance(String param1, String param2) {
-        ExplorerFragment fragment = new ExplorerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    ImageView imgAvatar;
+    TextView txtUsername, txtEmail;
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    private FragmentExplorerBinding binding;
+    private FirebaseDatabase database;
+    private Handler sliderHandler = new Handler();
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            binding.viewPager2.setCurrentItem(binding.viewPager2.getCurrentItem() + 1);
         }
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentExplorerBinding.inflate(inflater, container, false);
+        Mapping();
+        database = FirebaseDatabase.getInstance();
+        initBanner();
+        initTopMoving();
+        initUpcoming();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users").document(firebaseUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint({"SetTextI18n", "UseCompatLoadingForDrawables"})
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        String username = "Hello " + document.getString("first_name") +
+                                " " + document.getString("last_name");
+                        String email = document.getString("email");
+                        String linkImg = document.getString("imgAvatar");
+                        assert linkImg != null;
+                        if(linkImg.isEmpty()) {
+                            imgAvatar.setImageDrawable(getResources().getDrawable(R.drawable.baseline_account_circle_24));
+                        }
+                        else {
+                            Glide.with(requireContext())
+                                    .load(linkImg)
+                                    .circleCrop()
+                                    .into(imgAvatar);
+                        }
+                        txtUsername.setText(username);
+                        txtEmail.setText(email);
+                    }
+                }
+            }
+        });
+        return binding.getRoot();
+    }
+
+    private void Mapping() {
+        imgAvatar = binding.imgAvatar;
+        txtUsername = binding.txtUsername;
+        txtEmail = binding.txtEmail;
+    }
+
+    private void initUpcoming() {
+        DatabaseReference myRef = database.getReference("Upcomming");
+        binding.progressUpcoming.setVisibility(View.VISIBLE);
+        ArrayList<Film> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        items.add(issue.getValue(Film.class));
+                    }
+                    if (!items.isEmpty()) {
+                        binding.recyclerViewUpcoming.setLayoutManager(new LinearLayoutManager(getContext(),
+                                LinearLayoutManager.HORIZONTAL, false));
+                        binding.recyclerViewUpcoming.setAdapter(new FilmListAdapter(items));
+                    }
+                    binding.progressUpcoming.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initTopMoving() {
+        DatabaseReference myRef = database.getReference("Items");
+        binding.progressBarTop.setVisibility(View.VISIBLE);
+        ArrayList<Film> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        items.add(issue.getValue(Film.class));
+                    }
+                    if (!items.isEmpty()) {
+                        binding.recyclerViewTopMovies.setLayoutManager(new LinearLayoutManager(getContext(),
+                                LinearLayoutManager.HORIZONTAL, false));
+                        binding.recyclerViewTopMovies.setAdapter(new FilmListAdapter(items));
+                    }
+                    binding.progressBarTop.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initBanner() {
+        DatabaseReference myRef = database.getReference("Banners");
+        binding.progressBarBanner.setVisibility(View.VISIBLE);
+        ArrayList<SliderItems> items = new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        items.add(issue.getValue(SliderItems.class));
+                    }
+                    banners(items);
+                    binding.progressBarBanner.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void banners(ArrayList<SliderItems> items) {
+        binding.viewPager2.setAdapter(new SlidersAdapter(items, binding.viewPager2));
+        binding.viewPager2.setClipToPadding(false);
+        binding.viewPager2.setClipChildren(false);
+        binding.viewPager2.setOffscreenPageLimit(3);
+        binding.viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+        binding.viewPager2.setPageTransformer(compositePageTransformer);
+        binding.viewPager2.setCurrentItem(1);
+        binding.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable, 2000);
+            }
+        });
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explorer, container, false);
+    public void onPause() {
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable, 2000);
     }
 }
--- Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Adapters/FilmListAdapter.java
+++ Movies/app/src/main/java/ntu/duongthianhhong/blueymovies_64130758/Adapters/FilmListAdapter.java
@@ -1,74 +1,74 @@
-//package ntu.duongthianhhong.blueymovies_64130758.Adapters;
-//
-//import android.content.Context;
-//import android.content.Intent;
-//import android.view.LayoutInflater;
-//import android.view.View;
-//import android.view.ViewGroup;
-//import android.widget.ImageView;
-//import android.widget.TextView;
-//
-//import androidx.annotation.NonNull;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.bumptech.glide.Glide;
-//import com.bumptech.glide.load.resource.bitmap.CenterCrop;
-//import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-//import com.bumptech.glide.request.RequestOptions;
-//
-//import java.util.ArrayList;
-//
-//import ntu.duongthianhhong.blueymovies_64130758.Activities.DetailActivity;
-//import ntu.duongthianhhong.blueymovies_64130758.Domains.Film;
-//import ntu.duongthianhhong.blueymovies_64130758.R;
-//
-//public class FilmListAdapter extends RecyclerView.Adapter<FilmListAdapter.ViewHolder> {
-//   ArrayList <Film> items;
-//   Context context;
-//
-//    public FilmListAdapter(ArrayList<Film> items) {
-//        this.items = items;
-//    }
-//
-//    @NonNull
-//    @Override
-//    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        context = parent.getContext();
-//        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.film_viewholder,parent,false);
-//        return new ViewHolder(inflate);
-//    }
-//
-//    @Override
-//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-//        holder.titleTxt.setText(items.get(position).getTitle());
-//        RequestOptions requestOptions = new RequestOptions();
-//        requestOptions= requestOptions.transform(new CenterCrop(), new RoundedCorners(30));
-//        Glide.with(context)
-//                .load(items.get(position).getPoster())
-//                .apply(requestOptions)
-//                .into(holder.pic);
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(context, DetailActivity.class);
-//                intent.putExtra("object", items.get(position));
-//                context.startActivity(intent);
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public int getItemCount() {
-//        return items.size();
-//    }
-//
-//    public class ViewHolder extends RecyclerView.ViewHolder {
-//        TextView titleTxt;
-//        ImageView pic;
-//        public ViewHolder(@NonNull View itemView) {
-//            super(itemView);
-//            titleTxt = itemView.findViewById(R.id.nameTxt);
-//            pic = itemView.findViewById(R.id.pic);
-//        }
-//    }
-//}
+package ntu.duongthianhhong.blueymovies_64130758.Adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+import ntu.duongthianhhong.blueymovies_64130758.Activities.DetailActivity;
+import ntu.duongthianhhong.blueymovies_64130758.Domains.Film;
+import ntu.duongthianhhong.blueymovies_64130758.R;
+
+public class FilmListAdapter extends RecyclerView.Adapter<FilmListAdapter.ViewHolder> {
+   ArrayList <Film> items;
+   Context context;
+
+    public FilmListAdapter(ArrayList<Film> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        View inflate = LayoutInflater.from(parent.getContext()).inflate(R.layout.film_viewholder,parent,false);
+        return new ViewHolder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.titleTxt.setText(items.get(position).getTitle());
+        RequestOptions requestOptions = new RequestOptions();
+        requestOptions= requestOptions.transform(new CenterCrop(), new RoundedCorners(30));
+        Glide.with(context)
+                .load(items.get(position).getPoster())
+                .apply(requestOptions)
+                .into(holder.pic);
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("object", items.get(position));
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTxt;
+        ImageView pic;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt = itemView.findViewById(R.id.nameTxt);
+            pic = itemView.findViewById(R.id.pic);
+        }
+    }
+}
