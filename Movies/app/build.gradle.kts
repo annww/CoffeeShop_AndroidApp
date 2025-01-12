80353fe855788e63f6a27e5b34ac65768c23fd0a
Anh Hong <samh5813p@gmail.com>
2025-01-12 11:27:30 +0700

    update gradle

--- Movies/app/build.gradle.kts
+++ Movies/app/build.gradle.kts
@@ -35,33 +35,25 @@ android {
     buildFeatures {
         viewBinding = true
     }
-
-    dependencies {
-        implementation(libs.appcompat)
-        implementation(libs.material)
-        implementation(libs.activity)
-        implementation(libs.constraintlayout)
-        testImplementation(libs.junit)
-        androidTestImplementation(libs.ext.junit)
-        androidTestImplementation(libs.espresso.core)
-        implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
-        implementation("androidx.viewpager2:viewpager2:1.0.0")
-        implementation("com.github.bumptech.glide:glide:4.12.0")
-        implementation("com.github.Dimezis:BlurView:version-2.0.3")
-
-        // Sử dụng BOM Firebase để quản lý các phụ thuộc Firebase
-        implementation("com.google.firebase:firebase-database")
-        implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
-        implementation("com.google.firebase:firebase-auth")
-        implementation("com.google.firebase:firebase-firestore")
-        implementation("com.google.firebase:firebase-storage:20.3.0")
-    }
 }
 dependencies {
-    implementation(libs.firebase.database)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("com.github.ismaeldivita:chip-navigation-bar:1.4.0")
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
+
+    // Sử dụng BOM Firebase để quản lý các phụ thuộc Firebase
+    implementation("com.google.firebase:firebase-database")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
 }
 
